3f9066edc1ca3e34f9c3913b54970dd4bfb5706b
nikola-rados <nrados@uvic.ca>
2020-03-23 10:44:24 -0700

    Initial setup

--- thunderbird/processes/__init__.py
+++ thunderbird/processes/__init__.py
@@ -1,5 +1,7 @@
 from .wps_say_hello import SayHello
+from .wps_update_metadata import UpdateMetadata
 
 processes = [
     SayHello(),
+    UpdateMetadata(),
 ]
---
+++ thunderbird/processes/wps_update_metadata.py
@@ -0,0 +1,54 @@
+# Processor imports
+from pywps import (
+    Process,
+    ComplexInput,
+    FORMATS,
+)
+
+# Library imports
+import logging
+
+
+LOGGER = logging.getLogger("PYWPS")
+
+
+class UpdateMetadata(Process):
+    def __init__(self):
+        inputs = [
+            ComplexInput(
+                "netcdf",
+                "NetCDF",
+                abstract="NetCDF file to update",
+                min_occurs=1,
+                max_occurs=1,
+                supported_formats=[FORMATS.NETCDF],
+            ),
+            ComplexInput(
+                "updates",
+                "Updates",
+                abstract="JSON file containing updates",
+                min_occurs=1,
+                max_occurs=1,
+                supported_formats=[FORMATS.JSON],
+            )
+        ]
+        outputs = []
+
+        super(SayHello, self).__init__(
+            self._handler,
+            identifier="update_metadata",
+            title="Update Metadata",
+            abstract="Update NetCDF metadata from a YAML file.",
+            version="0.1.0",
+            inputs=inputs,
+            outputs=outputs,
+            store_supported=True,
+            status_supported=True,
+        )
+
+    @staticmethod
+    def _handler(request, response):
+        ncfile = request.inputs['netcdf'][0].file
+        updates = request.inputs['updates'][0].file
+        print(ncfile, updates)
+        return response
